e91d9fe487a54703c63c8c685f3eff973cd01561
pr4th4meshh <prathameshasolkar17@gmail.com>
2025-06-24 19:34:35 +0530

    feat: add clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SignedOut>
+          <div className="min-h-screen min-w-screen flex justify-center items-center">
+          <SignIn routing="hash" />
+          </div>
+        </SignedOut>
+        <SignedIn>
         {children}
+        </SignedIn>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
